3fbe4b39c4aa6bb744af7f98f75962e70c4c1ec5
CaioBrun0 <kmbmatos2@gmail.com>
2025-02-18 03:09:48 -0300

    Inicializando os pinos

---
+++ Conversor-adc/Conversor-adc.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"     
+#include "hardware/pwm.h"   
+#include "hardware/i2c.h"   
+#include "lib/ssd1306.h"   
+#include "lib/font.h"
+
+#define LED_a 12
+#define LED_r 13
+#define button 5
+#define VRX 26
+#define VRY 27
+#define button_joy 22
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+uint pwm_wrap = 4096;  
+
+#define WIDTH 128
+#define HEIGHT 64
+
+
+uint pwm_initialization_pin(uint pin, uint wrap){
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+
+    uint slice = pwm_gpio_to_slice_num(pin);
+    pwm_set_wrap(slice, wrap);
+    pwm_set_enabled(slice, wrap);
+    return slice;
+}
+
+void initialization(){
+    //inicialização do joystick
+    stdio_init_all();
+    adc_init();
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+
+    //inicialização do botão A
+    gpio_init(button);
+    gpio_set_dir(button, GPIO_OUT);
+    gpio_pull_up(button);
+
+    //Inicialização do botão do joystick
+    gpio_init(button_joy);
+    gpio_set_dir(button_joy, GPIO_IN);
+    gpio_pull_up(button_joy);
+
+    //Inicialização do LED RGB com o wrap
+    uint pwm_slice = pwm_initialization_pin(LED_r, pwm_wrap);
+    uint pwm_slice2 = pwm_initialization_pin(LED_a, pwm_wrap);
+
+}
+
+int map_value(int value, int in_min, int in_max, int out_min, int out_max) {
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+void gpio_irq_handler(uint gpio, uint32_t events){
+
+
+}
+
+
+int main()
+{
+    initialization();
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA); // Pull up the data line
+    gpio_pull_up(I2C_SCL); // Pull up the clock line
+    
+    ssd1306_t ssd; // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
+    int square_size = 8;
+    int square_x = (WIDTH - square_size) / 2;
+    int square_y = (HEIGHT - square_size) / 2;
+
+    uint32_t last_print_time = 0; 
+    bool cor = true;
+
+    gpio_set_irq_enabled_with_callback(button, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    while (true) {
+        adc_select_input(1);
+        uint16_t vrx_value = adc_read();
+        adc_select_input(0);
+        uint16_t vry_value = adc_read();
+
+
+
+        //Para controlar o PWM do LED vermelho no eixo X
+        if (vrx_value < 1810){
+            float factor = 1.0 - ((float)vrx_value / 2010.0);
+            uint pwm_value = 2010 + (pwm_wrap - 2010) * pow(factor, 2.5);
+            pwm_set_gpio_level(LED_r, pwm_value);
+        } else if (vrx_value > 1890) {
+            pwm_set_gpio_level(LED_r, vrx_value);
+        }else {
+            pwm_set_gpio_level(LED_r, 0);
+        }
+
+        
+        //Para controlar o PWM do LED azul no eixo y
+        if (vry_value < 2010){
+            float factor = 1.0 - ((float)vry_value / 2010.0);
+            uint pwm_value = 2010 + (pwm_wrap - 2010) * pow(factor, 2.5);
+            pwm_set_gpio_level(LED_a, pwm_value);
+        } else if (vry_value > 2090) {
+            pwm_set_gpio_level(LED_a, vry_value);
+        }else {
+            pwm_set_gpio_level(LED_a, 0);
+        }
+        
+        ssd1306_fill(&ssd, false);
+        ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor); // Desenha um retângulo
+        ssd1306_send_data(&ssd); // Atualiza o display
+        
+
+        float duty_cycle = (vry_value / 495.0) * 100;
+
+
+        uint32_t current_time = to_ms_since_boot(get_absolute_time());  
+        if (current_time - last_print_time >= 1000) {  
+            printf("VRy: %u\n", vry_value); 
+            printf("VRx: %u\n", vrx_value); 
+            printf("Duty Cycle LED: %.2f%%\n", duty_cycle); 
+            last_print_time = current_time;  
+        }
+        
+        
+        sleep_ms(100);
+    }
+}
+
